7b8adddd4930448bb581368e6b8a1464bc5a32aa
Juan Cuellar <juanchokuellar@gmail.com>
2025-07-11 13:09:08 -0500

    feat: add DownloadButton component for exporting referrals as Excel file

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,17 +1,28 @@
 import React from "react";
+import * as XLSX from "xlsx";
 import { Button } from "./ui/button";
+import { Download } from "lucide-react";
+import type { Referral } from "@/types/referral";
 
-function DownloadButton() {
+interface Props {
+  referrals: Referral[]
+}
+
+function DownloadButton({ referrals }: Props) {
   const handleDownload = () => {
-    // Logic to handle download
-    console.log("Download initiated");
+    const worksheet = XLSX.utils.json_to_sheet(referrals);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Referrals");
+    XLSX.writeFile(workbook, "referrals.xlsx");
   };
 
   return (
     <div className="flex justify-end items-center h-full">
       <Button
         onClick={handleDownload}
+        variant="outline"
       >
+        <Download className="mr-2" />
         Download
       </Button>
     </div>
